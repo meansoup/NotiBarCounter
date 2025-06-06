2fbc2b286d2e1fe111d87d9f9844dd64a55fd02e
meansoup <alsrnr1210@gmail.com>
2025-06-07 00:18:16 +0900

    feat: 히스토리 페이지 뷰 정리

--- app/src/main/java/com/example/notibarcounter/HistoryAdapter.java
+++ app/src/main/java/com/example/notibarcounter/HistoryAdapter.java
@@ -13,48 +13,44 @@ import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
-public class HistoryAdapter extends RecyclerView.Adapter<HistoryAdapter.HistoryViewHolder> {
+public class HistoryAdapter extends RecyclerView.Adapter<HistoryAdapter.ViewHolder> {
+    private List<NotificationHistoryItem> historyItems;
+    private SimpleDateFormat dateFormat;
 
-    private final List<NotificationHistoryItem> historyList;
-
-    public HistoryAdapter(List<NotificationHistoryItem> historyList) {
-        this.historyList = historyList;
+    public HistoryAdapter(List<NotificationHistoryItem> historyItems) {
+        this.historyItems = historyItems;
+        this.dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault());
     }
 
     @NonNull
     @Override
-    public HistoryViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.item_history, parent, false);
-        return new HistoryViewHolder(view);
+        return new ViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull HistoryViewHolder holder, int position) {
-        NotificationHistoryItem item = historyList.get(position);
-        holder.bind(item);
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        NotificationHistoryItem item = historyItems.get(position);
+        String formattedDate = dateFormat.format(new Date(item.getTimestamp()));
+        holder.timeTextView.setText(formattedDate);
+        holder.countTextView.setText(String.valueOf(item.getCount()));
     }
 
     @Override
     public int getItemCount() {
-        return historyList.size();
+        return historyItems.size();
     }
 
-    static class HistoryViewHolder extends RecyclerView.ViewHolder {
-        TextView timestampTextView;
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        TextView timeTextView;
         TextView countTextView;
 
-        HistoryViewHolder(@NonNull View itemView) {
+        public ViewHolder(@NonNull View itemView) {
             super(itemView);
-            timestampTextView = itemView.findViewById(R.id.historyTimestamp);
-            countTextView = itemView.findViewById(R.id.historyCount);
-        }
-
-        void bind(NotificationHistoryItem item) {
-            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault());
-            String formattedDate = sdf.format(new Date(item.getTimestamp()));
-            timestampTextView.setText("리셋 시간: " + formattedDate);
-            countTextView.setText("카운트: " + item.getCount());
+            timeTextView = itemView.findViewById(R.id.timeTextView);
+            countTextView = itemView.findViewById(R.id.countTextView);
         }
     }
 } 
